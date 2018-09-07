7adfb05260ebe382f5d127cb7eee8d8abe71ddc6
lakfa <fatiha.lakhmor@gmail.com>
2018-09-07 23:57:46 +0100

    code refacto and jsdoc

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,25 +9,44 @@ export class HomePage {
   result = '0';
   numbers = [];
   separators = ['\\\+', '-', 'x', '/'];
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController) {}
 
-  }
+  /**
+   * + click handler.
+   */
+  
   addition() {
-    this.result = this.result+ '+';
+    this.result = this.result + '+';
   }
-  
+
+  /**
+   * - click handler.
+   */
+
   substraction() {
-    this.result = this.result+ '-';
+    this.result = this.result + '-';
 
   }
-  
+
+  /**
+   * X click handler.
+   */
+
   multiplication() {
-    this.result = this.result+ 'x';
+    this.result = this.result + 'x';
   }
-  
+
+  /**
+   * / click handler.
+   */
+
   division() {
-    this.result = this.result+ '/';
-  } 
+    this.result = this.result + '/';
+  }
+
+  /**
+   * = click handler.
+   */
 
   doCalcul() {
     let finalValue = 0;
@@ -37,81 +56,59 @@ export class HomePage {
     // Create numbers array
     this.numbers = this.result.split(new RegExp(this.separators.join('|'), 'g'));
     finalValue = Number(this.numbers[0])
-    for(let i=0;i<this.numbers.length -1; i++) {
+    for (let i = 0; i < this.numbers.length - 1; i++) {
       // create operators array
       operators[i] = this.result[this.numbers[i].length + visitedString + i];
       visitedString += this.numbers[i].length;
       switch (operators[i]) {
         case '+':
-        finalValue += Number(this.numbers[i+1]) ;
-        break;
-
+          finalValue += Number(this.numbers[i + 1]);
+          break;
         case '-':
-        finalValue -= Number(this.numbers[i+1]) ;
-        break;
+          finalValue -= Number(this.numbers[i + 1]);
+          break;
         case 'x':
-        finalValue *= Number(this.numbers[i+1]) ;
-        break;
-
+          finalValue *= Number(this.numbers[i + 1]);
+          break;
         case '/':
-        if(this.numbers[i+1] === 0) return
-        finalValue /= Number(this.numbers[i+1]) ;
-        break;
-
+          if (this.numbers[i + 1] === 0) return
+          finalValue /= Number(this.numbers[i + 1]);
+          break;
         default:
           break;
       }
     }
     this.result = String(finalValue);
-
   }
 
+  /**
+   * On number click handler.
+   * @param n {number}
+   */
 
+  onNumber(n) {
+    if (this.result !== '0') {
+      this.result = this.result + String(n);
+    } else {
+      this.result = String(n);
+    }
+  }
 
+  /**
+   * On 0 click handler.
+   */
   on0() {
-     if(this.result !== '0') {
-      this.result = this.result+ '0';
+    if (this.result !== '0') {
+      this.result = this.result + '0';
     }
+  }
 
-  }
-  on1() { 
-    if(this.result === '0') {
-      this.result = '1';
-    }
-    else {
-      this.result = this.result+ '1';
-    }
-  }
-  on2() {
-    this.result = this.result + '2';
-  }
-  on3() {
-    this.result = this.result + '3';
-  }
-  on4() {
-    this.result = this.result + '4';
-   }
-  on5() {
-    this.result = this.result + '5';
-   }
-  on6() {
-    this.result = this.result + '6';
-   }
-  on7() { 
-    this.result = this.result + '7';
-  }
-  on8() {
-    this.result = this.result + '8';
-   }
-  on9() {
-    this.result = this.result + '9';
-   }
+  /**
+   * On C click handler.
+   */
 
   onC() {
     this.result = '0';
   }
-
-
-
 
 }
