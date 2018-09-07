98c61f1c9f9a33895c1d2d76cbb711b7331b03bd
lakfa <fatiha.lakhmor@gmail.com>
2018-09-07 23:02:55 +0100

    operations draft

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,34 +6,111 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-  result = 0;
+  result = '0';
+  numbers = [];
+  separators = ['\\\+', '-', 'x', '/'];
   constructor(public navCtrl: NavController) {
 
   }
   addition() {
-    debugger
+    this.result = this.result+ '+';
   }
   
-  substraction() {}
+  substraction() {
+    this.result = this.result+ '-';
+
+  }
   
-  multiplication() {}
+  multiplication() {
+    this.result = this.result+ 'x';
+  }
   
-  division() {} 
+  division() {
+    this.result = this.result+ '/';
+  } 
+
+  doCalcul() {
+    let finalValue = 0;
+    let operators = [];
+    let visitedString = 0;
+
+    // Create numbers array
+    this.numbers = this.result.split(new RegExp(this.separators.join('|'), 'g'));
+    finalValue = Number(this.numbers[0])
+    for(let i=0;i<this.numbers.length -1; i++) {
+      // create operators array
+      operators[i] = this.result[this.numbers[i].length + visitedString + i];
+      visitedString += this.numbers[i].length;
+      switch (operators[i]) {
+        case '+':
+        finalValue += Number(this.numbers[i+1]) ;
+        break;
+
+        case '-':
+        finalValue -= Number(this.numbers[i+1]) ;
+        break;
+        case 'x':
+        finalValue *= Number(this.numbers[i+1]) ;
+        break;
+
+        case '/':
+        if(this.numbers[i+1] === 0) return
+        finalValue /= Number(this.numbers[i+1]) ;
+        break;
+
+        default:
+          break;
+      }
+    }
+    this.result = String(finalValue);
+
+  }
+
+
 
   on0() {
-    this.result = 0;
+     if(this.result !== '0') {
+      this.result = this.result+ '0';
+    }
+
   }
   on1() { 
-    this.result = 1;
+    if(this.result === '0') {
+      this.result = '1';
+    }
+    else {
+      this.result = this.result+ '1';
+    }
   }
-  on2() { }
-  on3() { }
-  on4() { }
-  on5() { }
-  on6() { }
-  on7() { }
-  on8() { }
-  on9() { }
+  on2() {
+    this.result = this.result + '2';
+  }
+  on3() {
+    this.result = this.result + '3';
+  }
+  on4() {
+    this.result = this.result + '4';
+   }
+  on5() {
+    this.result = this.result + '5';
+   }
+  on6() {
+    this.result = this.result + '6';
+   }
+  on7() { 
+    this.result = this.result + '7';
+  }
+  on8() {
+    this.result = this.result + '8';
+   }
+  on9() {
+    this.result = this.result + '9';
+   }
+
+  onC() {
+    this.result = '0';
+  }
+
 
 
 
